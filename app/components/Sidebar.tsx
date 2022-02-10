604fef7c9e54effad177c4d22ba728156eff458e
Niklas Linnanen <nlinnanen@gmail.com>
2022-02-10 20:05:27 +0200

    add global colors

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,21 +1,21 @@
-import type { NextPage } from 'next'
-import SidebarButton from '../components/SidebarButton'
-import { FiHome, FiBarChart2, FiSearch } from 'react-icons/fi';
+import type { NextPage } from "next";
+import SidebarButton from "../components/SidebarButton";
+import { FiHome, FiBarChart2, FiSearch } from "react-icons/fi";
 
 const Sidebar: NextPage = () => {
-  return(
-    <div className="h-screen w-36 bg-blue-900 fixed">
-      <SidebarButton to='/'>
-        <FiHome size='42'/>
+  return (
+    <div className="h-screen w-36 fixed bg-prodekoBlue">
+      <SidebarButton to="/">
+        <FiHome size="42" />
       </SidebarButton>
-      <SidebarButton to='/stats'>
-        <FiBarChart2 size='42'/>
+      <SidebarButton to="/stats">
+        <FiBarChart2 size="42" />
       </SidebarButton>
-      <SidebarButton to='/search'>
-        <FiSearch size='42'/>
+      <SidebarButton to="/search">
+        <FiSearch size="42" />
       </SidebarButton>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
