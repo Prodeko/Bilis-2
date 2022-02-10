87b1a3cbcc0c0308cdb65613e99b2a99834ddfd4
Niklas Linnanen <nlinnanen@gmail.com>
2022-02-10 19:40:18 +0200

    add global tailwind colors

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      prodekoBlue: "#002851",
+      backgrounWhite: "#F0F0F2",
+      textGrey: "#434347",
+    },
     extend: {},
   },
   plugins: [],
