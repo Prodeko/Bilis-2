1245a5af1aa01a9a06af2887ecacebb14d6eb83c
villewebbitiimi <ville.ahokas@aalto.fi>
2022-02-10 22:09:21 +0200

    Sidebar search button expanding

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,18 +1,34 @@
 import type { NextPage } from "next";
+import React, { useState } from 'react'
 import SidebarButton from "../components/SidebarButton";
 import { FiHome, FiBarChart2, FiSearch } from "react-icons/fi";
 
 
 const Sidebar: NextPage = () => {
+  const[expandPartial, setExpandPartial] = useState(false)
+  const[expandFull, setExpandFull] = useState(false)
+
+  const expandP = () => {
+    setExpandPartial(true)
+  }
+
+  const expandF = () => {
+    setExpandFull(true)
+  }
+
+  const shrink = () => {
+    setExpandPartial(false)
+  }
+
   return (
-    <div className="h-screen w-36 fixed bg-prodekoBlue">
-      <SidebarButton to="/">
+    <div className={`h-screen w-36 fixed bg-prodekoBlue ${expandPartial? 'w-40' : 'w-36'} ${expandFull? 'w-64' : 'w-36'}`}>
+      <SidebarButton onMouseEnter={() => {}} onMouseLeave={() => {}} onClick={() => {}} to="/">
         <FiHome size="42" />
       </SidebarButton>
-      <SidebarButton to="/stats">
+      <SidebarButton onMouseEnter={() => {}} onMouseLeave={() => {}} onClick={() => {}} to="/stats">
         <FiBarChart2 size="42" />
       </SidebarButton>
-      <SidebarButton to="/search">
+      <SidebarButton onMouseEnter={expandP} onMouseLeave={shrink} onClick={expandF} to="/">
         <FiSearch size="42" />
       </SidebarButton>
     </div>
--- app/components/SidebarButton.tsx
+++ app/components/SidebarButton.tsx
@@ -4,13 +4,20 @@ import Link from 'next/link'
 type Props = {
   to: string,
   children: JSX.Element
+  onMouseEnter: () => void
+  onMouseLeave: () => void
+  onClick: () => void
 }
 
 
-const SidebarButton: NextPage<Props> = ({to, children}) => {
-  return(
+
+const SidebarButton: NextPage<Props> = ({ to, children, onMouseEnter, onMouseLeave, onClick }) => {
+  return (
     <Link href={to} passHref={true}>
-      <div className="bg-white rounded-[48px] hover:rounded-[42px] w-24 h-24 hover:w-28 hover:h-28 mx-auto my-4 flex items-center justify-center duration-200 shadow-2xl transition-background hover:cursor-pointer">
+      <div className="item bg-gray-100 hover:bg-blue-50 rounded-[48px] hover:rounded-[42px] w-24 h-24 hover:w-28 hover:h-28 mx-auto my-4 flex items-center justify-center duration-200 shadow-2xl transition-background hover:cursor-pointer"
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        onClick={onClick}>
         {children}
       </div>
     </Link>
