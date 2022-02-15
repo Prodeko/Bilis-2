a853f7d3dc0df82e5db2c6aa22acfd7ebf034247
muusitalo <mUusitalo@users.noreply.github.com>
2022-02-15 16:32:53 +0200

    Add winner selection box

---
+++ app/components/WinnerSelectionBox.tsx
@@ -0,0 +1,13 @@
+import CurrentPlayerButton from "./CurrentPlayerButton";
+
+const WinnerSelectionBox = () => {
+  return (
+    <div className="flex flex-row flex-nowrap mx-2 justify-evenly items-center">
+      <CurrentPlayerButton handleClick={() => {}} playerName='Teemu Teekkari' playerId="123" />
+      <p>vs</p>
+      <CurrentPlayerButton handleClick={() => {}} playerName='Tiina Teekkari' playerId="456" />
+    </div>
+  )
+}
+
+export default WinnerSelectionBox
