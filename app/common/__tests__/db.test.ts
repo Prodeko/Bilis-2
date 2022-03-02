7d97a189420e1af38145e76e190276600f3f9f21
Taavi Mustajoki <taavi.mustajoki@aalto.fi>
2022-03-02 18:40:23 +0200

    Fix tests

--- app/common/__tests__/db.test.ts
+++ app/common/__tests__/db.test.ts
@@ -2,73 +2,74 @@ import { Player } from "../types";
 import { playerAPI } from "../db/players";
 
 const testPlayers = [
-	{
-		firstName: "Teemu",
-		lastName: "Teekkari",
-		favoriteColor: "#21aaaf",
-	},
-	{
-		firstName: "Teuvo",
-		lastName: "Teekkari",
-		favoriteColor: "#ffffff",
-	},
-	{
-		firstName: "Tove",
-		lastName: "Tutalainen",
-		favoriteColor: "#ffaa00",
-	},
+  {
+    firstName: "Teemu",
+    lastName: "Teekkari",
+    favoriteColor: "#21aaaf",
+  },
+  {
+    firstName: "Teuvo",
+    lastName: "Teekkari",
+    favoriteColor: "#ffffff",
+  },
+  {
+    firstName: "Tove",
+    lastName: "Tutalainen",
+    favoriteColor: "#ffaa00",
+  },
 ];
 
 describe("Database", () => {
-	let testPlayer1: Player;
-	const [rawTestPlayer1, ...rawOtherTestPlayers] = testPlayers;
-	it("should create player", async () => {
-		testPlayer1 = await playerAPI.addPlayer(
-			rawTestPlayer1.firstName,
-			rawTestPlayer1.lastName,
-			rawTestPlayer1.favoriteColor
-		);
-		expect(testPlayer1).toMatchObject(rawTestPlayer1);
-	});
+  let testPlayer1: Player;
+  const [rawTestPlayer1, ...rawOtherTestPlayers] = testPlayers;
+  it("should create player", async () => {
+    testPlayer1 = await playerAPI.addPlayer(
+      rawTestPlayer1.firstName,
+      rawTestPlayer1.lastName,
+      "",
+      rawTestPlayer1.favoriteColor
+    );
+    expect(testPlayer1).toMatchObject(rawTestPlayer1);
+  });
 
-	it("should query player by id", async () => {
-		const res = await playerAPI.getById(testPlayer1.id);
+  it("should query player by id", async () => {
+    const res = await playerAPI.getById(testPlayer1.id);
 
-		expect(res).toMatchObject(testPlayer1);
-	});
+    expect(res).toMatchObject(testPlayer1);
+  });
 
-	it("should query full player stats", async () => {
-		const res = await playerAPI.getPlayerStatsById(testPlayer1.id);
+  it("should query full player stats", async () => {
+    const res = await playerAPI.getPlayerStatsById(testPlayer1.id);
 
-		expect(res).toMatchObject({
-			...testPlayer1,
-			wonGames: 0,
-			lostGames: 0,
-			maxElo: testPlayer1.elo,
-			minElo: testPlayer1.elo,
-		});
-	});
+    expect(res).toMatchObject({
+      ...testPlayer1,
+      wonGames: 0,
+      lostGames: 0,
+      maxElo: testPlayer1.elo,
+      minElo: testPlayer1.elo,
+    });
+  });
 
-	let testPlayer2: Player;
-	let testPlayer3: Player;
+  let testPlayer2: Player;
+  let testPlayer3: Player;
 
-	it("should create more players", async () => {
-		const createPlayer = async (p: {
-			firstName: string;
-			lastName: string;
-			favoriteColor: string;
-		}) => playerAPI.addPlayer(p.firstName, p.lastName, p.favoriteColor);
+  it("should create more players", async () => {
+    const createPlayer = async (p: {
+      firstName: string;
+      lastName: string;
+      favoriteColor: string;
+    }) => playerAPI.addPlayer(p.firstName, p.lastName, "", p.favoriteColor);
 
-		testPlayer2 = await createPlayer(testPlayers[1]);
-		testPlayer3 = await createPlayer(testPlayers[2]);
+    testPlayer2 = await createPlayer(testPlayers[1]);
+    testPlayer3 = await createPlayer(testPlayers[2]);
 
-		console.log(testPlayer2);
+    console.log(testPlayer2);
 
-		expect(testPlayer2).toMatchObject(testPlayers[1]);
-		expect(testPlayer3).toMatchObject(testPlayers[2]);
-	});
+    expect(testPlayer2).toMatchObject(testPlayers[1]);
+    expect(testPlayer3).toMatchObject(testPlayers[2]);
+  });
 
-	/*
+  /*
 	it("should return object in reversed adding order", async () => {
 		const res = await playerAPI.searchByKeywords("");
 
