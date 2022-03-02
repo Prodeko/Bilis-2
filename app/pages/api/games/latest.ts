82e3a08d8b190b28867d639e3e0edf6f43608eaf
nualn <nuuttinikkola1@gmail.com>
2022-03-02 18:36:48 +0200

    Add api endpoint for latest games

---
+++ app/pages/api/games/latest.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as gameAPI from "../../../common/db/games";
+import withAPIMiddleware from "../../../common/middleware";
+import { MethodNotAllowedError } from "../../../common/exceptions";
+
+const handleLatestGames = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const {
+    method,
+    body
+  } = req;
+
+  switch (method) {
+    case "GET":
+      res
+        .status(200)
+        .json(
+          await gameAPI.getLatestGames()
+        );
+      break;
+
+    case undefined: // this case is probably not possible but let's keep linter happy
+      throw new MethodNotAllowedError("", ["GET", "POST"]);
+    default:
+      throw new MethodNotAllowedError(method, ["GET", "POST"]);
+  }
+}
+
+
+export default withAPIMiddleware(handleLatestGames)
