586112f15ccea467a489d1fc25a4d850fa4059b2
Taavi Mustajoki <taavi.mustajoki@aalto.fi>
2022-03-02 18:48:52 +0200

    Fix Akselis Python-sekoilu

--- app/common/validators/index.ts
+++ app/common/validators/index.ts
@@ -1,45 +1,40 @@
 import { ParseError, ValidationError } from "../exceptions";
 
-const validateRequest = function (input: string, schema: Record<string, Function>) {    
-    // Function to parse the POST/PUT request body and validate it using a given schema.
-    try {
-        var parsed_input = JSON.parse(input)
+const validateRequest = (input: string, schema: Record<string, Function>) => {
+  // Function to parse the POST/PUT request body and validate it using a given schema.
+  try {
+    var parsedInput = JSON.parse(input);
+  } catch (error) {
+    // Catch expected errors from internal API
+    if (error instanceof SyntaxError) {
+      throw new ParseError("Invalid JSON input: " + error.message);
+    } else {
+      throw error;
     }
-    catch (error) {
-        // Catch expected errors from internal API
-        if (error instanceof SyntaxError) {
-            throw new ParseError("Invalid JSON input: "+error.message)
-        }
-        else {
-            throw error
-        }
-    }
-    
-    for (const spec_field in schema) {
-      if (typeof parsed_input[spec_field as keyof typeof parsed_input] == "undefined") {
-          throw new ValidationError(spec_field, "Missing field.")
-      }
-    }
-    for(const field in parsed_input) {
-      let validator = schema[field as keyof typeof schema]
-      if (typeof validator == "undefined") {
-          throw new ValidationError(field, "Unknown field.")
-      }
-      else if (!validator(parsed_input[field as keyof typeof parsed_input])) {
-          throw new ValidationError(field, "Invalid input.")
-      }
-    }
-    return parsed_input
   }
 
-// General type validators for request data:
-const stringValidator = function(input: any) {
-    return input.length > 3 && input.length < 100
-}
-const hexColorValidator = function (hex: string) {
-    return typeof hex === 'string'
-        && hex.length === 6
-        && !isNaN(Number('0x' + hex))
-}
+  for (const specField in schema) {
+    if (
+      typeof parsedInput[specField as keyof typeof parsedInput] == "undefined"
+    ) {
+      throw new ValidationError(specField, "Missing field.");
+    }
+  }
+  for (const field in parsedInput) {
+    let validator = schema[field as keyof typeof schema];
+    if (typeof validator == "undefined") {
+      throw new ValidationError(field, "Unknown field.");
+    } else if (!validator(parsedInput[field as keyof typeof parsedInput])) {
+      throw new ValidationError(field, "Invalid input.");
+    }
+  }
+  return parsedInput;
+};
 
-export {validateRequest, stringValidator, hexColorValidator}
+// General type validators for request data:
+const stringValidator = (input: any) => input.length > 3 && input.length < 100;
+
+const hexColorValidator = (hex: string) =>
+  typeof hex === "string" && hex.length === 6 && !isNaN(Number("0x" + hex));
+
+export { validateRequest, stringValidator, hexColorValidator };
