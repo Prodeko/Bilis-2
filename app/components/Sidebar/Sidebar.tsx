1971cc3b6c799386bead948dac72f909cf57678f
Sakari Palojoki <palojoki.sakari@gmail.com>
2022-03-10 19:50:38 +0200

    Further improve sidebar

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -21,7 +21,7 @@ const Sidebar: NextPage = () => {
   }
 
   return (
-    <div className={`flex flex-col h-screen w-36 fixed bg-prodekoBlue ${expandPartial ? 'w-52' : 'w-36'} ${expandFull ? 'w-1/4' : 'w-36'} duration-200 transition-background z-10`}>
+    <div className={`flex flex-col h-screen w-36 fixed bg-prodekoBlue ${expandPartial ? 'w-52' : 'w-36'} ${expandFull ? 'w-1/4' : 'w-36'} duration-200 transition-background z-20 shadow-2xl`}>
       <div className="flex-none">
         <SidebarButton to="/">
           <FiHome size="42" />
