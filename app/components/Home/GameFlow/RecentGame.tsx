1bc33f5ad8931038673e84953ec41e839ea5841c
Niklas Linnanen <nlinnanen@gmail.com>
2022-05-02 19:53:12 +0300

    Make under table visible in recents

--- app/components/Home/GameFlow/RecentGame.tsx
+++ app/components/Home/GameFlow/RecentGame.tsx
@@ -3,14 +3,20 @@ import Card from "./Card";
 
 
 const RecentGame = ({ game }: {game: GameListItem}) => {
+  const loserEmoji = String.fromCodePoint(parseInt(game.winner.emoji.slice(2)))
+  const winnerEmoji = String.fromCodePoint(parseInt(game.loser.emoji.slice(2)))
   return (
     <Card>
       <div>{game.datetime.toString().slice(11, 16)}</div>
-      <div style={{background: game.winner.favoriteColor}} className={`shadow-l w-8 h-8 p-4 rounded-full flex justify-center items-center text-xl`}>😍</div>
+      <div style={{background: game.winner.favoriteColor}} className={`shadow-l w-8 h-8 p-4 rounded-full flex justify-center items-center text-xl`}>
+        {loserEmoji}
+      </div>
       <div>{`${game.winner.firstName} ${game.winner.lastName}`}</div>
       <div>{game.winnerElo.toFixed(0)}</div>
-      <div>VS</div>
-      <div style={{background: game.loser.favoriteColor}} className={`shadow-l w-8 h-8 p-4 rounded-full flex justify-center items-center text-xl`}>😍</div>
+      <div>VS {game.underTable ? String.fromCodePoint(128169) : ' '}</div>
+      <div style={{background: game.loser.favoriteColor}} className={`shadow-l w-8 h-8 p-4 rounded-full flex justify-center items-center text-xl`}>
+        {winnerEmoji}  
+      </div>
       <div>{`${game.loser.firstName} ${game.loser.lastName}`}</div>
       <div>{game.loserElo.toFixed(0)}</div>
     </Card>
