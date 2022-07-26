12768e8fe80acc4720195453c7b3e4189b4da5cd
Leevi Jäkälä <jakala.leevi@gmail.com>
2022-07-26 18:07:40 +0300

    Add helper to create css class string dynamically

--- app/common/utils/helperFunctions.ts
+++ app/common/utils/helperFunctions.ts
@@ -1,1 +1,6 @@
+import type { Styles } from '@common/types'
 export const logWithBase = (x: number, base: number): number => Math.log(x) / Math.log(base)
+
+export const createCssClasses = (styles: Styles, baseClass: string, variation: string): string => {
+  return `${styles[baseClass]} ${styles[`${baseClass}__${variation}`]}}`
+}
