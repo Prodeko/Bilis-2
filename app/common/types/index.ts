bc17a1480d4ecbdc585554aa5041b7d3d7dd93d1
Leevi Jäkälä <jakala.leevi@gmail.com>
2022-07-27 20:26:55 +0300

    Remove unused types

--- app/common/types/index.ts
+++ app/common/types/index.ts
@@ -11,41 +11,10 @@ type Player = {
   lastName: string
   nickname: string
   emoji: string
-  favoriteColor: string
   elo: number
 }
 
 type NewPlayer = Omit<Player, 'id'>
-
-type PlayerWithStats = Player & {
-  wonGames: number
-  lostGames: number
-  maxElo: number
-  minElo: number
-}
-
-type Game = {
-  id: number
-  datetime: Date
-  underTable: boolean
-  winnerElo: number
-  loserElo: number
-  winner: Player
-  loser: Player
-}
-
-type GameListItem = Game & {
-  winner: Player
-  loser: Player
-  winnerEloBefore: number
-  loserEloBefore: number
-}
-
-type MutualStatsPlayer = {
-  mutualGamesWon: number
-  name: string
-  favoriteColor: string
-}
 
 interface RequestWithPage extends NextApiRequest {
   page?: number
@@ -56,14 +25,4 @@ type Styles = {
   readonly [key: string]: string
 }
 
-export type {
-  Player,
-  Game,
-  ValidationError,
-  PlayerWithStats,
-  NewPlayer,
-  GameListItem,
-  RequestWithPage,
-  MutualStatsPlayer,
-  Styles,
-}
+export type { Player, ValidationError, NewPlayer, RequestWithPage, Styles }
