34e0e417df8324288bced7fb4a415778fcc832f6
Leevi Jäkälä <jakala.leevi@gmail.com>
2022-07-27 19:44:40 +0300

    Add basic styling for leaderboard

--- app/components/03-organisms/Leaderboard/index.tsx
+++ app/components/03-organisms/Leaderboard/index.tsx
@@ -1,9 +1,11 @@
 import Paragraph from '@atoms/Paragraph'
+import { getCssClass } from '@common/utils/helperFunctions'
+import styles from './Leaderboard.module.scss'
 import LeaderboardCard from '@molecules/LeaderboardCard'
 
 const Leaderboard = (): JSX.Element => {
   return (
-    <div className="leaderboard-card">
+    <div className={getCssClass(styles)}>
       <Paragraph variation="l">All time leaderboard</Paragraph>
       <LeaderboardCard position={1} points={3600} emoji={'😜'} name="Leevi Jäkälä" />
       <LeaderboardCard position={2} points={1200} emoji={'😜'} name="Leevi Jäkälä" />
