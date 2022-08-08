cf5b16bbc575e2602166c93724d8677096893d14
Sakari Palojoki <palojoki.sakari@gmail.com>
2022-08-08 18:30:04 +0300

    Create profile route and prepare layout

---
+++ app/pages/player/[id].tsx
@@ -0,0 +1,12 @@
+import ProfileLayout from '@components/Layouts/ProfileLayout'
+import type { NextPage } from 'next'
+
+const PlayerPage: NextPage = () => {
+  return (
+    <ProfileLayout>
+      <h1>Welcome to profile page!</h1>
+    </ProfileLayout>
+  )
+}
+
+export default PlayerPage
