edcc065b2d7e25387795312b0e0ab6d0d67f6b78
Leevi Jäkälä <jakala.leevi@gmail.com>
2022-09-19 19:43:42 +0300

    Render AddGame in app index

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import type { NextPage } from 'next'
 
-import type { HomeLeaderboard, RecentGame } from '@common/types'
+import type { HomeLeaderboard, Player, RecentGame } from '@common/types'
 import Header from '@components/Homepage/Header'
 import Leaderboard from '@components/Homepage/Leaderboard'
 import Queue from '@components/Homepage/Queue'
@@ -9,18 +9,21 @@ import Recents from '@components/Homepage/Recents'
 import HomeGrid from '@components/Layout/HomeLayout/HomeGrid'
 import HomeLayout from '@components/Layout/HomeLayout/HomeLayout'
 import { NEXT_PUBLIC_API_URL } from '@config/index'
+import AddGame from '@components/AddGame'
 
 interface Props {
   leaderboard: HomeLeaderboard
   recentGames: RecentGame[]
+  players: Player[]
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Home: NextPage<Props> = ({ leaderboard, recentGames }: Props) => {
+const Home: NextPage<Props> = ({ leaderboard, recentGames, players }: Props) => {
   return (
     <HomeLayout>
       <Header />
       <HomeGrid>
+        <AddGame players={players} />
         <Leaderboard leaderboard={leaderboard} />
         <Queue />
         <Recents recentGames={recentGames} />
@@ -30,14 +33,15 @@ const Home: NextPage<Props> = ({ leaderboard, recentGames }: Props) => {
 }
 
 export async function getServerSideProps() {
-  const [leaderboard, recentGames] = (
+  const [leaderboard, recentGames, players] = (
     await Promise.all([
       axios.get(`${NEXT_PUBLIC_API_URL}/leaderboard`),
       axios.get(`${NEXT_PUBLIC_API_URL}/game/recents`),
+      axios.get(`${NEXT_PUBLIC_API_URL}/player/latest`),
     ])
   ).map(result => result.data)
 
-  return { props: { leaderboard, recentGames } }
+  return { props: { leaderboard, recentGames, players } }
 }
 
 export default Home
