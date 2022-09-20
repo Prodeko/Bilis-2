b2ff840c84ba95c589f90bdf196c10511ab555c5
Niklas Linnanen <nlinnanen@gmail.com>
2022-09-20 19:52:49 +0300

    Add player info to queue item

--- app/components/Queue/QueueItem/index.tsx
+++ app/components/Queue/QueueItem/index.tsx
@@ -1,10 +1,20 @@
+import { Player } from '@common/types'
 import { FunctionComponent } from 'react'
 import styles from './QueueItem.module.scss'
 
-interface QueueItemProps {}
+interface QueueItemProps {
+  place: number
+  player: Player
+}
 
-const QueueItem: FunctionComponent<QueueItemProps> = () => {
-  return <div>item</div>
+const QueueItem: FunctionComponent<QueueItemProps> = ({ place, player }) => {
+  return (
+    <div>
+      <div>{place}</div>
+      <div>{`${player.firstName} ${player.lastName}`}</div>
+      <button type="button">remove</button>
+    </div>
+  )
 }
 
 export default QueueItem
