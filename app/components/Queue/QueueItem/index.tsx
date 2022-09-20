10b758a50c436dcfa320c114b444eae0243acebf
Niklas Linnanen <nlinnanen@gmail.com>
2022-09-20 19:30:45 +0300

    initialize components

---
+++ app/components/Queue/QueueItem/index.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from 'react'
+import styles from './QueueItem.module.scss'
+
+interface QueueItemProps {}
+
+const QueueItem: FunctionComponent<QueueItemProps> = () => {
+  return <div>item</div>
+}
+
+export default QueueItem
---
+++ app/components/Queue/index.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from 'react'
+import styles from './Queue.module.scss'
+
+interface QueueProps {}
+
+const Queue: FunctionComponent<QueueProps> = () => {
+  return <div>queue</div>
+}
+
+export default Queue
