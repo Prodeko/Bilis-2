a0bb15c8b8553c13015af95459ce29f9b1d73187
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2022-09-20 23:27:52 +0300

    add Header component

---
+++ app/components/Homepage/Header/index.tsx
@@ -0,0 +1,31 @@
+import QuoteCard from '@components/Profile/ProfileHeader/QuoteCard'
+import Filter from '@components/utility/Filter'
+import Image from 'next/image'
+import billiardPic from '../../../public/images/billiard.jpg'
+import styles from './Header.module.scss'
+
+const Header = () => {
+  // TODO!! Remove hard coding
+  const text = 'Jääbelis'
+  const author = "Petri 'Raikku' Ranta"
+
+  return (
+    <header className={styles.header}>
+      <Image
+        src={billiardPic}
+        alt="Billiard Table"
+        objectPosition="0px -1600px"
+        layout="fill"
+        objectFit="cover"
+      />
+      <Filter>
+        <div className={styles.layout}>
+          <h1 className={styles.title}>Biliskilke</h1>
+          <QuoteCard text={text} author={author} />
+        </div>
+      </Filter>
+    </header>
+  )
+}
+
+export default Header
