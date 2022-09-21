9b19e79eb8d834df1e401168f2b37d83d970efec
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2022-09-21 18:50:37 +0300

    add Card component

---
+++ app/components/utility/Card/index.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react'
+import styles from './Card.module.scss'
+
+interface Props {
+  children: ReactNode
+  colspan: string
+  rowspan: string
+}
+
+const Card = ({ children, colspan, rowspan }: Props) => {
+  const inlineStyles = {
+    gridColumn: colspan,
+    gridRow: rowspan,
+  }
+
+  return (
+    <div style={inlineStyles} className={styles.card}>
+      <div className={styles.cardgrid}>{children}</div>
+    </div>
+  )
+}
+
+export default Card
