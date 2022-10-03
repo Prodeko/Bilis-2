74ee2b57cd880a88131c7271f87d88dda4cc9c5e
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2022-10-03 23:49:14 +0300

    add query for getting player stats by id

--- app/server/db/games/index.ts
+++ app/server/db/games/index.ts
@@ -1,4 +1,5 @@
-import { GameWithPlayers, NewGame } from '@common/types'
+import type { PlayerStats, GameWithPlayers, NewGame } from '@common/types'
+import { DEFAULT_ELO } from '@common/utils/constants'
 import { Game, Player } from '@server/models'
 import { getPlayerById, updatePlayerById } from '@server/db/players'
 import { getScoreChange } from '@common/utils/gameStats'
@@ -18,6 +19,39 @@ const getWinGameCountForPlayer = async (playerId: number) => {
       winnerId: playerId,
     },
   })
+}
+
+const getPlayerStats = async (playerId: number): Promise<PlayerStats> => {
+  const games = await Game.findAll({
+    where: {
+      [Op.or]: [{ winnerId: playerId }, { loserId: playerId }],
+    },
+  })
+  const jsonGames = games.map(game => game.toJSON()) as GameWithPlayers[]
+
+  const totalGames = jsonGames.length
+  const wonGames = jsonGames.filter(game => game.winnerId === playerId).length
+  const lostGames = totalGames - wonGames
+  const winPercentage = totalGames === 0 ? 0 : (wonGames / totalGames) * 100
+
+  // Helper function to add elos from games
+  const pickElo = (game: GameWithPlayers, id: number) =>
+    game.winnerId === id ? game.winnerEloAfter : game.loserEloAfter
+
+  const eloData: Array<number> = [DEFAULT_ELO] // Everybody starts from 400 elo
+  if (totalGames !== 0) {
+    // Avoid 500 errors
+    const elos = jsonGames.map(game => pickElo(game, playerId))
+    eloData.push(...elos)
+  }
+
+  return {
+    wonGames,
+    lostGames,
+    totalGames,
+    winPercentage,
+    eloData,
+  }
 }
 
 const getLatestGames = async (n = 20): Promise<GameWithPlayers[]> => {
@@ -76,6 +110,7 @@ export {
   createGame,
   getGameCountForPlayer,
   getWinGameCountForPlayer,
+  getPlayerStats,
   getLatestGames,
   clearGamesDEV,
 }
