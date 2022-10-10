de9aa38183ad37176bab395e56220247a467af6d
Sakari Palojoki <palojoki.sakari@gmail.com>
2022-10-10 18:54:21 +0300

    Create route for new profile and layout for it

---
+++ app/pages/player/new.tsx
@@ -0,0 +1,11 @@
+import NewProfileLayout from '@components/Layout/ProfileLayout/newProfileLayout'
+
+const newPlayer = () => {
+  return (
+    <NewProfileLayout>
+      <h1>Create new profile</h1>
+    </NewProfileLayout>
+  )
+}
+
+export default newPlayer
