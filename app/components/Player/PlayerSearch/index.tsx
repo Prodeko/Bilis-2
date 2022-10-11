886921581931a791196a3f54c1c16eaa34ebafb1
Niklas Linnanen <nlinnanen@gmail.com>
2022-10-11 13:38:39 +0300

    Add arrow key navigation

--- app/components/Player/PlayerSearch/index.tsx
+++ app/components/Player/PlayerSearch/index.tsx
@@ -1,7 +1,15 @@
 import { Player } from '@common/types'
 import usePlayers from 'hooks/usePlayers'
 import Link from 'next/link'
-import { FunctionComponent, useState } from 'react'
+import Router from 'next/router'
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  FunctionComponent,
+  KeyboardEventHandler,
+  useState,
+} from 'react'
+import { useStateManager } from 'react-select'
 import styles from './PlayerSearch.module.scss'
 
 interface PlayerSearchProps {
@@ -11,6 +19,31 @@ interface PlayerSearchProps {
 const PlayerSearch: FunctionComponent<PlayerSearchProps> = ({ initialPlayers: players }) => {
   const [query, setQuery] = useState<string>('')
   const [isVisible, setIsVisible] = useState<boolean>(false)
+  const [selectedIdx, setSelectedIdx] = useState<number>(0)
+
+  const filteredPlayers = players.filter(p => new RegExp(query, 'i').test(p.firstName + p.lastName))
+
+  const handleKeyPress: KeyboardEventHandler<HTMLInputElement> = e => {
+    switch (e.key) {
+      case 'ArrowUp':
+        setSelectedIdx(Math.max(0, selectedIdx - 1))
+        break
+
+      case 'ArrowDown':
+        setSelectedIdx(Math.min(filteredPlayers.length, selectedIdx + 1))
+        break
+
+      case 'Enter':
+        const id = filteredPlayers[selectedIdx].id
+        Router.push(`player/${id}`)
+        break
+    }
+  }
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = e => {
+    setQuery(e.target.value)
+    setSelectedIdx(0)
+  }
 
   return (
     <div className={styles.container}>
@@ -20,17 +53,18 @@ const PlayerSearch: FunctionComponent<PlayerSearchProps> = ({ initialPlayers: pl
         onClick={() => {
           if (!isVisible) setIsVisible(true)
         }}
-        onChange={e => setQuery(e.target.value)}
+        onKeyDown={handleKeyPress}
+        onChange={handleChange}
       />
       <div className={styles.results}>
         {isVisible &&
-          players
-            .filter(p => new RegExp(query, 'i').test(p.firstName + p.lastName))
-            .map(player => (
-              <Link href={`/player/${player.id}`}>
-                <div className={styles.player}>{`${player.firstName} ${player.lastName}`}</div>
-              </Link>
-            ))}
+          filteredPlayers.map((player, i) => (
+            <Link href={`/player/${player.id}`} key={player.id}>
+              <div
+                className={`${styles.player} ${selectedIdx === i ? styles.selected : ''}`}
+              >{`${player.firstName} ${player.lastName}`}</div>
+            </Link>
+          ))}
       </div>
     </div>
   )
