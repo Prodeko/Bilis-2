9899dfc1da3fa37050cfd43f268ff18324b4aa13
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2022-10-16 13:17:10 +0300

    add new API endpoint for fetching mutual stats

---
+++ app/pages/api/player/mutual-stats.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { isNumber } from '@common/types/guards'
+import { getPlayerById } from '@server/db/players'
+import { getMutualGames } from '@server/db/games'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    const currentPlayerId = req.query.id1 as unknown
+    const opposingPlayerId = req.query.id2 as unknown
+
+    if (isNumber(currentPlayerId) && isNumber(opposingPlayerId)) {
+      const [currentPlayer, opposingPlayer] = await Promise.all([
+        getPlayerById(currentPlayerId),
+        getPlayerById(opposingPlayerId),
+      ])
+
+      if (!currentPlayer) {
+        res.status(404).json({ error: `No player found with ID ${currentPlayerId}` })
+      } else if (!opposingPlayer) {
+        res.status(404).json({ error: `No player found with ID ${opposingPlayerId}` })
+      } else {
+        const mutualGamesStats = await getMutualGames(currentPlayerId, opposingPlayerId)
+        res.status(200).json(mutualGamesStats)
+      }
+    } else {
+      res.status(400).json({ error: 'IDs must be type of number' })
+    }
+  }
+}
