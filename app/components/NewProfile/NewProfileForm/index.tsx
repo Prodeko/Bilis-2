2098bd7cac45ff3d2b890e56d7ad6ad0468b8b15
Sakari Palojoki <palojoki.sakari@gmail.com>
2022-10-17 19:39:57 +0300

    Add emoji selector and basic validation

--- app/components/NewProfile/NewProfileForm/index.tsx
+++ app/components/NewProfile/NewProfileForm/index.tsx
@@ -1,14 +1,17 @@
 import styles from './NewProfileForm.module.scss'
 import { useState } from 'react'
 import Field from './Field'
-import { BsGenderAmbiguous } from 'react-icons/bs'
+import { BsFillPersonPlusFill } from 'react-icons/bs'
 import React from 'react'
+import EmojiPicker from 'emoji-picker-react'
 
 const NewProfileForm = () => {
   const [firstName, setFirstName] = useState<string>('')
   const [lastName, setLastName] = useState<string>('')
   const [nickname, setNickname] = useState<string>('')
   const [motto, setMotto] = useState<string>('')
+  const [emoji, setEmoji] = useState<string>('')
+  const [emojiSelectorOpen, setEmojiSelectorOpen] = useState<boolean>(false)
 
   const submitNewPlayer = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
@@ -16,25 +19,63 @@ const NewProfileForm = () => {
     console.log({ firstName, lastName, nickname, motto })
   }
 
+  const isValid =
+    firstName !== '' &&
+    lastName !== '' &&
+    nickname !== '' &&
+    motto !== '' &&
+    emoji !== '' &&
+    emojiSelectorOpen === false
+
   return (
     <div className={styles.container}>
       <div className={styles.helperWrapper}>
         <h1 className={styles.title}>New profile</h1>
         <form className={styles.form}>
-          <Field placeholder="Teemu" value={firstName} setValue={setFirstName} label="First name" />
-          <Field placeholder="Teekkari" value={lastName} setValue={setLastName} label="Last name" />
-          <Field placeholder="Teksa" value={nickname} setValue={setNickname} label="Nickname" />
-          <Field
-            placeholder="Ei tänään, eikä huomenna."
-            value={motto}
-            setValue={setMotto}
-            label="Motto"
-          />
-          <button className={styles.button} onClick={submitNewPlayer}>
-            Create player
-            <BsGenderAmbiguous className={styles.icon} />
-          </button>
+          <div className={styles.fields}>
+            <Field
+              placeholder="Teemu"
+              value={firstName}
+              setValue={setFirstName}
+              label="First name"
+            />
+            <Field
+              placeholder="Teekkari"
+              value={lastName}
+              setValue={setLastName}
+              label="Last name"
+            />
+            <Field placeholder="Teksa" value={nickname} setValue={setNickname} label="Nickname" />
+            <Field
+              placeholder="Ei tänään, eikä huomenna."
+              value={motto}
+              setValue={setMotto}
+              label="Motto"
+            />
+          </div>
+          {emojiSelectorOpen ? (
+            <EmojiPicker
+              onEmojiClick={emoji => {
+                setEmoji(emoji.emoji)
+                setEmojiSelectorOpen(false)
+              }}
+              height={280}
+              width={280}
+              previewConfig={{ showPreview: false }}
+            />
+          ) : (
+            <div onClick={() => setEmojiSelectorOpen(true)} className={styles.emojiCircle}>
+              {emoji === '' ? '?' : emoji}
+            </div>
+          )}
         </form>
+        <button
+          className={`${styles.button} ${isValid ? styles.buttonActive : styles.buttonInactive}`}
+          onClick={submitNewPlayer}
+        >
+          Create player
+          <BsFillPersonPlusFill className={styles.icon} />
+        </button>
       </div>
     </div>
   )
