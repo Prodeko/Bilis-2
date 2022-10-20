09a57ff25b0f044397536236b32ea6a320bb744c
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2022-10-20 20:36:59 +0300

    fix player search filtering only by one name

--- app/server/db/players/index.ts
+++ app/server/db/players/index.ts
@@ -4,7 +4,7 @@ import { Op } from 'sequelize'
 import { NewPlayer, PlayerExtended, Player as PlayerType } from '@common/types'
 import { Player } from '@server/models'
 
-import { getLatestGames } from '../games'
+import { getLatestGames } from '@server/db/games'
 
 const createPlayer = async (player: NewPlayer) => {
   // Ghetto validation
@@ -70,26 +70,48 @@ const getLatestPlayers = async (n = 20) => {
   return uniquePlayers.slice(0, n)
 }
 
-const searchPlayers = async (query: string): Promise<Player[]> => {
-  const players = await Player.findAll({
-    where: {
-      [Op.or]: [
-        {
-          firstName: {
-            [Op.iLike]: `%${query}%`,
+const searchPlayers = async (searchWords: string[]): Promise<Player[]> => {
+  const findValidPlayers = async (query: string) => {
+    const players = await Player.findAll({
+      where: {
+        [Op.or]: [
+          {
+            firstName: {
+              [Op.iLike]: `%${query}%`,
+            },
           },
-        },
-        {
-          lastName: {
-            [Op.iLike]: `%${query}%`,
+          {
+            lastName: {
+              [Op.iLike]: `%${query}%`,
+            },
           },
-        },
-      ],
-    },
+        ],
+      },
+    })
+    return players
+  }
+
+  // Returns valid players responding to each searchword
+  const validPlayersArrays = await Promise.all(searchWords.map(findValidPlayers))
+
+  const jsonPlayersArrays = validPlayersArrays.map(playerArr =>
+    playerArr.map(player => player.toJSON() as Player)
+  )
+
+  // Choose players that appear in each array
+  const [firstArray, ...remainingArrays] = jsonPlayersArrays
+  const validPlayers = firstArray.filter(firstPlayer => {
+    return remainingArrays.every(playerArr => {
+      return playerArr.some(anotherPlayer => {
+        return (
+          firstPlayer.firstName === anotherPlayer.firstName &&
+          firstPlayer.lastName === anotherPlayer.lastName
+        )
+      })
+    })
   })
 
-  const jsonPlayers = players.map(p => p.toJSON())
-  return jsonPlayers
+  return validPlayers
 }
 
 export {
