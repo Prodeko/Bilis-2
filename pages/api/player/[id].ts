22e23b12d603defca2fa47ce6eb3aeee8707155c
Leevi Jäkälä <jakala.leevi@gmail.com>
2022-11-01 19:51:01 +0200

    Refactor player endpoint

--- pages/api/player/[id].ts
+++ pages/api/player/[id].ts
@@ -1,32 +1,30 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { round } from 'lodash'
-import { isNumber } from '@common/types/guards'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 import type { Player } from '@common/types'
+import { isNumber } from '@common/types/guards'
 import { getPlayerStats } from '@server/db/games'
 import { getPlayerById } from '@server/db/players'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    const id = Number(req.query.id) as unknown
-
-    if (!isNumber(id)) {
-      res.status(400).json({ error: 'ID must be type of number' })
-      return
-    }
-
-    const [player, playerStats] = await Promise.all([getPlayerById(id), getPlayerStats(id)])
-    if (!player) {
-      res.status(404).json({ error: `No player found with ID ${id}` })
-      return
-    }
-
-    const jsonPlayer = player.toJSON() as Player
-    res.status(200).json({
-      ...jsonPlayer,
-      ...playerStats,
-      winPercentage: round(playerStats.winPercentage, 2).toFixed(2),
-      elo: round(jsonPlayer.elo, 2).toFixed(2),
-    })
+  const id = Number(req.query.id) as unknown
+  if (!isNumber(id)) {
+    return res.status(400).json({ error: 'ID must be type of number' })
   }
+
+  if (req.method === 'GET') {
+    const [player, playerStats] = await Promise.all([getPlayerById(id), getPlayerStats(id)])
+
+    if (player) {
+      const jsonPlayer = player.toJSON() as Player
+      return res.status(200).json({
+        ...jsonPlayer,
+        ...playerStats,
+        winPercentage: round(playerStats.winPercentage, 2).toFixed(2),
+        elo: round(jsonPlayer.elo, 2).toFixed(2),
+      })
+    }
+    return res.status(404).json({ error: `No player found with ID ${id}` })
+  }
+  return res.status(405)
 }
