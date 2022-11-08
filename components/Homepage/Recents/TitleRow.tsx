a93890015fbdb8eef9d10fd7cf6bc86f78dd2d74
Niklas Linnanen <nlinnanen@gmail.com>
2022-11-08 18:13:20 +0000

    Add remove logic

--- components/Homepage/Recents/TitleRow.tsx
+++ components/Homepage/Recents/TitleRow.tsx
@@ -1,10 +1,29 @@
+import { Game } from '@common/types'
+import { NEXT_PUBLIC_API_URL } from '@config/index'
+import axios from 'axios'
+import { NextApiResponse } from 'next'
 import styles from './Recents.module.scss'
 
 const TitleRow = () => {
+  const handleRemove = async () => {
+    window.alert('Remove the latest game?')
+    const { data } = await axios.delete(`${NEXT_PUBLIC_API_URL}/game/latest`)
+
+    if (typeof data == 'string') {
+      console.error(data)
+    } else if (typeof data == 'object') {
+      console.log(
+        `Removed game id ${data.id} with winner ${data.winnerId} and loser ${data.loserId}`
+      )
+    }
+  }
+
   return (
     <div className={styles.titleRow}>
       <h2 className={styles.title}>Recents</h2>
-      <button className={styles.removeButton}>Remove latest</button>
+      <button className={styles.removeButton} onClick={handleRemove}>
+        Remove latest
+      </button>
     </div>
   )
 }
