d4683527de37a8348923b5fed5cb055aad646c53
aksun1 <aksunpostit@gmail.com>
2022-11-08 19:28:39 +0000

    Notification initial design work

--- components/Layout/BaseLayout/index.tsx
+++ components/Layout/BaseLayout/index.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react'
 
 import styles from './BaseLayout.module.scss'
 import Sidebar from './Sidebar'
+import Notification from '../../utility/Notification'
 
 interface LayoutProps {
   children: ReactNode
@@ -9,9 +10,12 @@ interface LayoutProps {
 
 const BaseLayout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <div className={styles.grid}>
-      <Sidebar />
-      <main className={styles.layout}>{children}</main>
+    <div className={styles.grid_vertical}>
+      <Notification />
+      <div className={styles.grid}>
+        <Sidebar />
+        <main className={styles.layout}>{children}</main>
+      </div>
     </div>
   )
 }
