fae9e6ea8222ef2ef8173c823dee160500aae98e
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2022-11-12 17:18:12 +0000

    seperate PlayerSearch to Queue and Link componenents

--- components/utility/PlayerSearch/PlayerSearchLink/index.tsx
+++ components/utility/PlayerSearch/PlayerSearchLink/index.tsx
@@ -6,6 +6,8 @@
 
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import { ChangeEventHandler, useState } from 'react'
+import Router from 'next/router'
+import Link from 'next/link'
 
 import { Player } from '@common/types'
 import useKeyPress from '@hooks/useKeyPress'
@@ -13,22 +15,13 @@ import usePlayers from '@hooks/usePlayers'
 
 import styles from './PlayerSearchLink.module.scss'
 
-interface PlayerSearchLinkProps {
-  handleSelect: (e: Player) => void
-  placeholder?: string
-  filterFunction?: (e: Player) => boolean
-}
+const PlayerSearchLink = () => {
+  const getRoute = (id: number) => `player/${id}`
+  const handleSelect = ({ id }: Player) => Router.push(getRoute(id))
 
-const PlayerSearchLink = ({
-  placeholder = 'Search for a player',
-  handleSelect,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  filterFunction = (_p: Player) => true,
-}: PlayerSearchLinkProps) => {
   const [isVisible, setIsVisible] = useState<boolean>(false)
   const { players, setQuery } = usePlayers(400)
-  const filteredPlayers = players.filter(filterFunction ?? (() => true))
-  const { handleKeyPress, selectedIdx, setSelectedIdx } = useKeyPress(filteredPlayers, handleSelect)
+  const { handleKeyPress, selectedIdx, setSelectedIdx } = useKeyPress(players, handleSelect)
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = e => {
     setQuery(e.target.value)
@@ -40,25 +33,28 @@ const PlayerSearchLink = ({
     <div className={styles.container}>
       <input
         className={styles.search}
-        placeholder={placeholder}
+        placeholder={'Search for a player'}
         onClick={() => setIsVisible(true)}
         onKeyDown={handleKeyPress}
         onChange={handleChange}
         onBlur={() => setIsVisible(false)}
       />
-      <ul className={styles.results}>
-        {isVisible &&
-          filteredPlayers.map((player, i) => (
-            <li
-              key={player.id}
-              className={`${styles.player} ${selectedIdx === i ? styles.selected : ''}`}
-              onMouseDown={e => e.preventDefault()} // We need to block the onBlur effect first: https://stackoverflow.com/questions/17769005/onclick-and-onblur-ordering-issue/#57630197
-              onClick={() => handleSelect(filteredPlayers[i])}
-            >
-              {`${player.firstName} ${player.lastName}`}
-            </li>
+      {isVisible && (
+        <ul className={styles.results}>
+          {players.map((player, i) => (
+            <Link href={getRoute(player.id)} passHref>
+              <li
+                key={player.id}
+                className={`${styles.player} ${selectedIdx === i ? styles.selected : ''}`}
+                onMouseDown={e => e.preventDefault()} // We need to block the onBlur effect first: https://stackoverflow.com/questions/17769005/onclick-and-onblur-ordering-issue/#57630197
+                onClick={() => handleSelect(players[i])}
+              >
+                <a>{`${player.firstName} ${player.lastName}`}</a>
+              </li>
+            </Link>
           ))}
-      </ul>
+        </ul>
+      )}
     </div>
   )
 }
