16e5c43d0e23c2b68666576b7bded15f15fcdc86
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2022-11-21 19:04:51 +0000

    add elo meter limits, move currentelo position down

--- components/AddGameModal/Content/PlayerSelection/SelectedPlayer/EloMeter.tsx
+++ components/AddGameModal/Content/PlayerSelection/SelectedPlayer/EloMeter.tsx
@@ -1,6 +1,7 @@
 import type { Player } from '@common/types'
 import GaugeChart from 'react-gauge-chart'
 import styles from './SelectedPlayer.module.scss'
+import { round } from 'lodash'
 
 // API: https://github.com/Martin36/react-gauge-chart
 
@@ -10,17 +11,21 @@ const EloMeter = ({ player }: { player: Player }) => {
   const pointerPosition = player.elo / MAX_ELO
 
   return (
-    <GaugeChart
-      animateDuration={2000}
-      animDelay={100}
-      className={styles.gauge}
-      colors={['#CC0000', '#00BB00']} // Flip red and green colors
-      cornerRadius={1}
-      formatTextValue={(eloPercent: string) => (8 * Number(eloPercent)).toFixed(2)}
-      id={`gauge-chart-${player.id}`}
-      nrOfLevels={20}
-      percent={pointerPosition}
-    />
+    <div className={styles.gauge}>
+      <GaugeChart
+        animateDuration={2000}
+        animDelay={100}
+        colors={['#CC0000', '#00BB00']} // Flip red and green colors
+        cornerRadius={1}
+        hideText={true}
+        id={`gauge-chart-${player.id}`}
+        nrOfLevels={20}
+        percent={pointerPosition}
+      />
+      <span className={styles.currentelo}>{round(player.elo, 2)}</span>
+      <span className={styles.minelo}>{MIN_ELO}</span>
+      <span className={styles.maxelo}>{MAX_ELO}</span>
+    </div>
   )
 }
 
