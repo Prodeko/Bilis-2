62d61b36ddeb4a0efc7a463f2e316e07f32586b0
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2022-11-22 10:00:57 +0000

    integrate autoanimate to recents

--- components/Homepage/Recents/Table/TableBody.tsx
+++ components/Homepage/Recents/Table/TableBody.tsx
@@ -1,10 +1,12 @@
 import styles from './Table.module.scss'
 import RecentsRow from './RecentsRow'
 import { RecentGame } from '@common/types'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 const TableBody = ({ recentGames }: { recentGames: RecentGame[] }) => {
+  const [parent, _enableAnimations] = useAutoAnimate<HTMLTableSectionElement>()
   return (
-    <tbody className={styles.tablebody}>
+    <tbody ref={parent} className={styles.tablebody}>
       {recentGames.map(game => {
         return <RecentsRow key={game.id} game={game} />
       })}
