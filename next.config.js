19a9be6a67f5ca6b08bed7c1ed7f44565f2f1d54
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2022-12-02 20:14:10 +0200

    add production dockerfile

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  }
+  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
