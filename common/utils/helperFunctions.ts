cb7b23080993f46fb59dd27a46bf2782bafe3d9a
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2022-12-02 16:41:04 +0000

    fix playersearchlink permutations

--- common/utils/helperFunctions.ts
+++ common/utils/helperFunctions.ts
@@ -1,1 +1,27 @@
 export const logWithBase = (x: number, base: number): number => Math.log(x) / Math.log(base)
+
+export const permutator = <T>(inputArr: T[]) => {
+  /**
+   * Takes array as input and returns an array of the different permutations.
+   * @param {T[]} inputArr Input array, can be arbitrary type
+   * @return {T[][]}       Output permutations
+   */
+
+  let result: Array<T[]> = []
+
+  const permute = (arr: T[], m: T[] = []) => {
+    if (arr.length === 0) {
+      result.push(m)
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice()
+        let next = curr.splice(i, 1)
+        permute(curr.slice(), m.concat(next))
+      }
+    }
+  }
+
+  permute(inputArr)
+
+  return result
+}
