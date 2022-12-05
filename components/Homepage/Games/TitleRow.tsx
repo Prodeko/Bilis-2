72492fc432d61f44288045bb4c25e83c7f22a1ea
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2022-12-05 17:31:31 +0000

    add animations to deletion modal

--- components/Homepage/Games/TitleRow.tsx
+++ components/Homepage/Games/TitleRow.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 import { RecentGame } from '@common/types'
 import styles from './Games.module.scss'
@@ -11,11 +12,12 @@ interface Props {
 
 const TitleRow = ({ games, setGames }: Props) => {
   const [visible, setVisible] = useState<boolean>(false)
+  const [parent] = useAutoAnimate<HTMLDivElement>({ duration: 250 })
 
   const closeModal = () => setVisible(false)
 
   return (
-    <div className={styles.titleRow}>
+    <div ref={parent} className={styles.titleRow}>
       <h2 className={styles.title}>Games</h2>
       <button className={styles.removeButton} onClick={() => setVisible(true)}>
         Remove latest
