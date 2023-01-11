39c49c608951ae4b2d4b8adb6a0b458012eec1b5
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-01-11 13:10:40 +0000

    add tsdoc to helper functions

--- common/utils/helperFunctions.ts
+++ common/utils/helperFunctions.ts
@@ -2,15 +2,14 @@ import { Player, PlayerStats } from '@common/types'
 
 export const logWithBase = (x: number, base: number): number => Math.log(x) / Math.log(base)
 
+/**
+ * Takes array as input and returns an array of the different permutations.
+ * @param inputArr Input array, can be arbitrary type
+ *
+ * @return Output permutations
+ */
 export const permutator = <T>(inputArr: T[]) => {
-  /**
-   * Takes array as input and returns an array of the different permutations.
-   * @param {T[]} inputArr Input array, can be arbitrary type
-   * @return {T[][]}       Output permutations
-   */
-
   const result: Array<T[]> = []
-
   const permute = (arr: T[], m: T[] = []) => {
     if (arr.length === 0) {
       result.push(m)
@@ -24,10 +23,16 @@ export const permutator = <T>(inputArr: T[]) => {
   }
 
   permute(inputArr)
-
   return result
 }
 
+/**
+ * Formats player's full name give a player object
+ *
+ * @param player Player object
+ *
+ * @returns Formatted full name
+ */
 export const formatFullName = (player: Player) =>
   `${player.firstName} "${player.firstName}" ${player.lastName}`
 
