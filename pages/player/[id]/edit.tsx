eb1da6200eac9ede4ba57fdc5eddc9c0c9626b89
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-01-11 14:23:05 +0000

    Refactor getServerSideProps for player edit page

--- pages/player/[id]/edit.tsx
+++ pages/player/[id]/edit.tsx
@@ -1,26 +1,55 @@
-import axios from 'axios'
 import { GetServerSideProps } from 'next'
+import ErrorPage from 'next/error'
 
 import { Player } from '@common/types'
+import { isNumber } from '@common/types/guards'
 import NewProfileLayout from '@components/Layout/NewProfileLayout'
 import BackButton from '@components/NewProfile/BackButton'
 import ProfileForm from '@components/NewProfile/ProfileForm'
-import { NEXT_PUBLIC_API_URL } from '@config/index'
+import { getPlayerById } from '@server/db/players'
 
-const editPlayer = ({ player }: { player: Player }) => {
+type ErrorType = {
+  error: string
+  statusCode: number
+}
+
+type Props = Player | ErrorType
+
+const editPlayer = (props: Props) => {
+  if ('error' in props) {
+    return <ErrorPage title={props.error} statusCode={props.statusCode} />
+  }
+
   return (
     <NewProfileLayout>
-      <BackButton route={`/player/${player.id}`} />
-      <ProfileForm player={player} />
+      <BackButton route={`/player/${props.id}`} />
+      <ProfileForm player={props} />
     </NewProfileLayout>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const playerId = params?.id
-  const res = await axios.get(`${NEXT_PUBLIC_API_URL}/player/${playerId}`)
+export const getServerSideProps: GetServerSideProps = async context => {
+  const id = Number(context.query.id) as unknown
+  if (!isNumber(id)) {
+    return {
+      props: {
+        error: 'ID must be type of number',
+        statusCode: 400,
+      },
+    }
+  }
+  const player = await getPlayerById(id)
 
-  return { props: { player: res.data } }
+  if (!player) {
+    return {
+      props: {
+        error: `No player found with ID ${id}`,
+        statusCode: 404,
+      },
+    }
+  }
+
+  return { props: { player: player.toJSON() } }
 }
 
 export default editPlayer
