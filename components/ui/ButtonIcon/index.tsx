66cb1035e9a45ff8badaff98a72a57bbcc3e26c0
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-02-10 21:08:17 +0000

    add WIP ButtonIcon component

---
+++ components/ui/ButtonIcon/index.tsx
@@ -0,0 +1,21 @@
+import React, { ComponentProps } from 'react'
+import { IconType } from 'react-icons'
+
+import styles from './ButtonIcon.module.scss'
+
+export type ButtonProps = ComponentProps<'button'>
+
+type Variation = 'destructive' | 'close'
+
+interface Props extends ButtonProps {
+  Icon: IconType
+  variation: Variation
+}
+
+export const ButtonIcon = ({ Icon, variation, ...props }: Props): React.FC<Props> => {
+  return (
+    <button className={styles[variation]} {...props}>
+      {<Icon className={styles.icon} />}
+    </button>
+  )
+}
