4f2c6b04abe1d967b3b0972995e20d58b906a9f8
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-02-10 21:06:28 +0000

    add WIP Titlerow component

---
+++ components/ui/TitleRow/Title.tsx
@@ -0,0 +1,10 @@
+import type { Variation } from './'
+import styles from './TitleRow.module.scss'
+
+interface Props {
+  variation: Variation
+}
+
+export const Title = ({ variation }: Props) => {
+  return <h2 className={styles.title}>{variation}</h2>
+}
