da94dd7a586937ee0efb88d975ebf60f4926d65f
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-06-21 17:26:22 +0000

    add divprops to card

--- app/components/ui/Card/Card.tsx
+++ app/components/ui/Card/Card.tsx
@@ -1,17 +1,19 @@
-import { ReactNode } from 'react'
+import { ComponentProps, ReactNode } from 'react'
 
 import type { GridPosition } from '@common/types'
 
 import styles from './Card.module.scss'
 
-interface Props {
+type DivProps = ComponentProps<'div'>
+
+interface Props extends DivProps {
   children: ReactNode
   gridPosition?: GridPosition
 }
 
-export const Card = ({ children, gridPosition }: Props) => {
+export const Card = ({ children, gridPosition, ...props }: Props) => {
   return (
-    <div className={styles.card} style={gridPosition}>
+    <div {...props} className={styles.card} style={gridPosition}>
       {children}
     </div>
   )
