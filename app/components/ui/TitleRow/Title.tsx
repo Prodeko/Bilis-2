889d7630d03c54c7392743478004a0524a31c8e7
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-06-23 17:49:08 +0000

    app props and new variation to titlerow

--- app/components/ui/TitleRow/Title.tsx
+++ app/components/ui/TitleRow/Title.tsx
@@ -1,11 +1,18 @@
 import type { Variation } from '.'
+import { ComponentProps } from 'react'
 
 import styles from './TitleRow.module.scss'
 
-interface Props {
+type HeadingProps = ComponentProps<'h2'>
+
+interface Props extends HeadingProps {
   variation: Variation
 }
 
-export const Title = ({ variation }: Props) => {
-  return <h2 className={styles.title}>{variation}</h2>
+export const Title = ({ variation, ...props }: Props) => {
+  return (
+    <h2 {...props} className={styles.title}>
+      {variation}
+    </h2>
+  )
 }
