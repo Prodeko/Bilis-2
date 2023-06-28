7c9e79cb8b096666352d29a9a15111be83b686bd
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-06-28 17:37:43 +0000

    separate table Filter as its own component

--- app/components/ui/MultifunctionTable/index.tsx
+++ app/components/ui/MultifunctionTable/index.tsx
@@ -18,6 +18,7 @@ import {
   useReactTable,
 } from '@tanstack/react-table'
 
+import { Filter } from './Filter'
 import styles from './MultifunctionTable.module.scss'
 
 export interface PlayerTableSchema {
@@ -38,58 +39,6 @@ export interface GameTableSchema {
   loserFargoNow: number
   loserFargoDifference: number
   underTable: boolean
-}
-
-const Filter = ({ column, table }: { column: Column<any, any>; table: ReactTable<any> }) => {
-  const firstValue = table.getPreFilteredRowModel().flatRows[0]?.getValue(column.id)
-
-  const columnFilterValue = column.getFilterValue()
-
-  if (typeof firstValue === 'number') {
-    return (
-      <div className={styles.filterContainer}>
-        <input
-          type="number"
-          inputMode="numeric"
-          value={(columnFilterValue as [number, number])?.[0] ?? ''}
-          onChange={e =>
-            column.setFilterValue((old: [number, number]) => [e.target.value, old?.[1]])
-          }
-          placeholder={`Min`}
-          className={styles.numberInput}
-        />
-        <input
-          type="number"
-          inputMode="numeric"
-          value={(columnFilterValue as [number, number])?.[1] ?? ''}
-          onChange={e =>
-            column.setFilterValue((old: [number, number]) => [old?.[0], e.target.value])
-          }
-          placeholder={`Max`}
-          className={styles.numberInput}
-        />
-      </div>
-    )
-  } else if (typeof firstValue === 'string') {
-    return (
-      <input
-        type="text"
-        value={(columnFilterValue ?? '') as string}
-        onChange={e => column.setFilterValue(e.target.value)}
-        placeholder={`Search...`}
-        className={styles.textInput}
-      />
-    )
-  } else if (typeof firstValue === 'boolean') {
-    /**
-     * Implement filter where
-     * 1. Default: no filter
-     * 2. Filter - show UT: show only ut games
-     * 3. Filter - show no UT: show only non-ut games
-     *
-     */
-    // return <input type="checkbox" onChange={e => column.setFilterValue(e.target.checked)} />
-  }
 }
 
 interface Props<Schema> {
