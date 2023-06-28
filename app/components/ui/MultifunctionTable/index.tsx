a8db9292ffc313253aff35fac3b6aa7090b5d0c4
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-06-28 17:34:58 +0000

    disable undertable filter

--- app/components/ui/MultifunctionTable/index.tsx
+++ app/components/ui/MultifunctionTable/index.tsx
@@ -45,34 +45,51 @@ const Filter = ({ column, table }: { column: Column<any, any>; table: ReactTable
 
   const columnFilterValue = column.getFilterValue()
 
-  return typeof firstValue === 'number' ? (
-    <div className={styles.filterContainer}>
+  if (typeof firstValue === 'number') {
+    return (
+      <div className={styles.filterContainer}>
+        <input
+          type="number"
+          inputMode="numeric"
+          value={(columnFilterValue as [number, number])?.[0] ?? ''}
+          onChange={e =>
+            column.setFilterValue((old: [number, number]) => [e.target.value, old?.[1]])
+          }
+          placeholder={`Min`}
+          className={styles.numberInput}
+        />
+        <input
+          type="number"
+          inputMode="numeric"
+          value={(columnFilterValue as [number, number])?.[1] ?? ''}
+          onChange={e =>
+            column.setFilterValue((old: [number, number]) => [old?.[0], e.target.value])
+          }
+          placeholder={`Max`}
+          className={styles.numberInput}
+        />
+      </div>
+    )
+  } else if (typeof firstValue === 'string') {
+    return (
       <input
-        type="number"
-        inputMode="numeric"
-        value={(columnFilterValue as [number, number])?.[0] ?? ''}
-        onChange={e => column.setFilterValue((old: [number, number]) => [e.target.value, old?.[1]])}
-        placeholder={`Min`}
-        className={styles.numberInput}
+        type="text"
+        value={(columnFilterValue ?? '') as string}
+        onChange={e => column.setFilterValue(e.target.value)}
+        placeholder={`Search...`}
+        className={styles.textInput}
       />
-      <input
-        type="number"
-        inputMode="numeric"
-        value={(columnFilterValue as [number, number])?.[1] ?? ''}
-        onChange={e => column.setFilterValue((old: [number, number]) => [old?.[0], e.target.value])}
-        placeholder={`Max`}
-        className={styles.numberInput}
-      />
-    </div>
-  ) : (
-    <input
-      type="text"
-      value={(columnFilterValue ?? '') as string}
-      onChange={e => column.setFilterValue(e.target.value)}
-      placeholder={`Search...`}
-      className={styles.textInput}
-    />
-  )
+    )
+  } else if (typeof firstValue === 'boolean') {
+    /**
+     * Implement filter where
+     * 1. Default: no filter
+     * 2. Filter - show UT: show only ut games
+     * 3. Filter - show no UT: show only non-ut games
+     *
+     */
+    // return <input type="checkbox" onChange={e => column.setFilterValue(e.target.checked)} />
+  }
 }
 
 interface Props<Schema> {
