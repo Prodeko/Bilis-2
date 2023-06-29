a3b2efd4b0a376dcbc3b5abe5215e3a7cb736a95
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-06-29 18:28:57 +0000

    pre-determine table column widths

--- app/components/ui/MultifunctionTable/Table.tsx
+++ app/components/ui/MultifunctionTable/Table.tsx
@@ -7,6 +7,19 @@ interface Props<Schema> {
   table: ReactTable<Schema>
 }
 
+const dataTypeCellStyling = (cellValue: any) => {
+  const dataType = typeof cellValue
+  if (dataType === 'number') {
+    return styles.numberCell
+  } else if (dataType === 'string') {
+    return styles.stringCell
+  } else if (dataType === 'boolean') {
+    return styles.booleanCell
+  } else {
+    console.warn('Unknown datatype in the header')
+  }
+}
+
 export const Table = <Schema extends object>({ table }: Props<Schema>) => {
   return (
     <table className={styles.table}>
@@ -14,8 +27,15 @@ export const Table = <Schema extends object>({ table }: Props<Schema>) => {
         {table.getHeaderGroups().map(headerGroup => (
           <tr className={styles.headerRow} key={headerGroup.id}>
             {headerGroup.headers.map(header => {
+              const firstValue = table
+                .getPreFilteredRowModel()
+                .flatRows[0]?.getValue(header.column.id)
               return (
-                <th className={styles.headerCell} key={header.id} colSpan={header.colSpan}>
+                <th
+                  className={dataTypeCellStyling(firstValue)}
+                  key={header.id}
+                  colSpan={header.colSpan}
+                >
                   {header.isPlaceholder ? null : (
                     <div className={styles.headerContainer}>
                       {flexRender(header.column.columnDef.header, header.getContext())}
