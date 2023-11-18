15ef2d3473553b1396250de7a8a04311b95a08d3
Niklas Linnanen <nlinnanen@gmail.com>
2023-11-18 18:52:35 +0000

    Control seasonal mode from toggle

--- app/components/ui/Sidebar/SeasonToggle.tsx
+++ app/components/ui/Sidebar/SeasonToggle.tsx
@@ -5,21 +5,21 @@ import useSeasonalMode from '@hooks/useSeasonalMode'
 import styles from './Sidebar.module.scss'
 
 const SeasonToggle: React.FC = () => {
-  const seasonalMode = useSeasonalMode()
+  const { seasonal, toggleSeasonalMode } = useSeasonalMode()
 
   return (
     <div className={styles.seasonToggleContainer}>
       <label className={styles.seasonToggleLabel}>
         <input
           type="checkbox"
-          checked={seasonalMode}
-          onChange={() => console.log('toggle')}
+          checked={seasonal}
+          onChange={toggleSeasonalMode}
           className={styles.seasonToggleSwitch}
         />
         <span className={styles.seasonToggleSlider}></span>
       </label>
       <span className={styles.seasonToggleText}>
-        {seasonalMode ? 'Seasonal Mode On' : 'Seasonal Mode Off'}
+        {seasonal ? 'Seasonal Mode On' : 'Seasonal Mode Off'}
       </span>
     </div>
   )
--- hooks/useSeasonalMode.ts
+++ hooks/useSeasonalMode.ts
@@ -1,9 +1,14 @@
 import { useSearchParams } from 'next/navigation'
 
-const useSeasonalMode = (): boolean => {
+const useSeasonalMode = (): { seasonal: boolean; toggleSeasonalMode: () => void } => {
   const params = useSearchParams()
+  const seasonal = params.get('seasonal') === 'true'
 
-  return params.get('seasonal') === 'true'
+  const toggleSeasonalMode = () => {
+    window.location.replace(`?seasonal=${!seasonal}`)
+  }
+
+  return { seasonal, toggleSeasonalMode }
 }
 
 export default useSeasonalMode
