2f55c77206fb1241d7731c7a71ea710a43d123d6
Niklas Linnanen <nlinnanen@gmail.com>
2023-11-20 11:07:06 +0000

    Add season name to header

--- app/components/ui/Header/HeaderTitle.tsx
+++ app/components/ui/Header/HeaderTitle.tsx
@@ -1,5 +1,7 @@
 import { ComponentProps } from 'react'
 
+import { getCurrentSeason } from '@server/db/seasons'
+
 import styles from './Header.module.scss'
 
 type h1Props = ComponentProps<'h1'>
@@ -8,11 +10,15 @@ interface Props extends h1Props {
   title: string
 }
 
-const HeaderTitle = ({ title, ...props }: Props) => {
+const HeaderTitle = async ({ title, ...props }: Props) => {
+  const season = await getCurrentSeason()
   return (
-    <h1 {...props} className={styles.title}>
-      {title}
-    </h1>
+    <div className={styles.titleContainer}>
+      <h1 {...props} className={styles.title}>
+        {title}
+      </h1>
+      {season && <h2 className={styles.seasonTitle}>Season: {season.name}</h2>}
+    </div>
   )
 }
 
