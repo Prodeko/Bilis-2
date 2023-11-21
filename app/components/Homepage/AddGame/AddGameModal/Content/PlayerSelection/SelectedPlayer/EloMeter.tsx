b93f221bc91e661cfda1eb5c2a7e9e5037feb878
Niklas Linnanen <nlinnanen@gmail.com>
2023-11-21 14:53:04 +0000

    Fix elometer to show season elo

--- app/components/Homepage/AddGame/AddGameModal/Content/PlayerSelection/SelectedPlayer/EloMeter.tsx
+++ app/components/Homepage/AddGame/AddGameModal/Content/PlayerSelection/SelectedPlayer/EloMeter.tsx
@@ -3,15 +3,18 @@ import { memo } from 'react'
 import GaugeChart from 'react-gauge-chart'
 
 import type { Player } from '@common/types'
+import useSeasonalMode from '@hooks/useSeasonalMode'
 
 import styles from './SelectedPlayer.module.scss'
 
 // API: https://github.com/Martin36/react-gauge-chart
 
 const EloMeter = ({ player }: { player: Player }) => {
+  const { seasonal } = useSeasonalMode()
+  const elo = seasonal ? player.seasonElo ?? 400 : player.elo
   const MIN_ELO = 0
   const MAX_ELO = 800
-  const pointerPosition = player.elo / MAX_ELO
+  const pointerPosition = elo / MAX_ELO
 
   return (
     <div className={styles.gauge}>
@@ -26,7 +29,7 @@ const EloMeter = ({ player }: { player: Player }) => {
         nrOfLevels={20}
         percent={pointerPosition}
       />
-      <span className={styles.currentelo}>{round(player.elo, 2)}</span>
+      <span className={styles.currentelo}>{round(elo, 2)}</span>
       <span className={styles.minelo}>{MIN_ELO}</span>
       <span className={styles.maxelo}>{MAX_ELO}</span>
     </div>
