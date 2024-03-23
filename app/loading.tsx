eb61c8f511894a4795f088a777c984bad798d7ef
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-03-23 19:28:37 +0000

    Refactor and restyle loading spinner

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import LoadingSpinner from "@components/ui/LoadingSpinner";
+import { LoadingSpinner } from "@components/ui/LoadingSpinner";
 
 const LoadingPage = () => {
   return <LoadingSpinner />;
