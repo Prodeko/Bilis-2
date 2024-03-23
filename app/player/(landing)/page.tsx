2629a914031971aa07f4e6fbd8207d3e5766f0db
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-03-23 21:47:40 +0000

    Refactor player landing page

--- app/player/(landing)/page.tsx
+++ app/player/(landing)/page.tsx
@@ -1,37 +1,48 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
+import { cva } from "class-variance-authority";
+import { useState } from "react";
 
-import PlayerSearchLink from '@ui/PlayerSearch/PlayerSearchLink'
+import PlayerSearchLink from "@ui/PlayerSearch/PlayerSearchLink";
 
-import styles from './PlayerLanding.module.scss'
+const styles = cva(
+  "max-h-1/2 grid w-3/4 grid-rows-[auto_minmax(0,_1fr)] gap-4 text-left transition-all duration-[0.7s]",
+  {
+    variants: {
+      extended: {
+        true: "min-h-[75%]",
+        false: "min-h-[25%]",
+      },
+    },
+  },
+);
 
 const PlayerPage = () => {
-  const [extended, setExtended] = useState<boolean>(false)
-  const [visible, setVisible] = useState<boolean>(false)
+  const [extended, setExtended] = useState<boolean>(false);
+  const [visible, setVisible] = useState<boolean>(false);
 
   const onBlur = () => {
-    setVisible(false)
-    setTimeout(() => setExtended(false), 400) // The same length as extendind transformation in PlayerSearchLink.moudule.scss %resultContainer placeholder class
-  }
+    setVisible(false);
+    setTimeout(() => setExtended(false), 400); // The same length as extendind transformation in PlayerSearchLink.moudule.scss %resultContainer placeholder class
+  };
 
   const onClick = () => {
     if (!extended) {
-      setExtended(true)
-      setTimeout(() => setVisible(true), 700) // // The same length as extendind transformation in PlayerSearchLink.moudule.scss %resultContainer placeholder class
+      setExtended(true);
+      setTimeout(() => setVisible(true), 700); // // The same length as extendind transformation in PlayerSearchLink.moudule.scss %resultContainer placeholder class
     }
-  }
+  };
 
   return (
-    <div className={styles.wrapper}>
-      <div className={extended ? styles.searchContainer__extended : styles.searchContainer}>
-        <h1 className={styles.header}>Player Search</h1>
+    <div className="flex h-full items-center justify-center p-12">
+      <div className={styles({ extended })}>
+        <h1 className="text-5xl font-semibold text-white">Player Search</h1>
         <PlayerSearchLink visible={visible} onClick={onClick} onBlur={onBlur} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PlayerPage
+export default PlayerPage;
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
