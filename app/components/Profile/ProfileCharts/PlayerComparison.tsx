a87086544c725e0d341303adc40360e8dc2dd947
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-03-23 11:53:42 +0000

    Refactor PieChart

--- app/components/Profile/ProfileCharts/PlayerComparison.tsx
+++ app/components/Profile/ProfileCharts/PlayerComparison.tsx
@@ -1,24 +1,32 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
+import { useState } from "react";
 
-import type { PieChartProps } from '@common/types'
-import { useAutoAnimate } from '@formkit/auto-animate/react'
-import PieChart from '@ui/PieChart'
-import PlayerSearchSelect from '@ui/PlayerSearch/PlayerSearchSelect'
+import PlayerSearchSelect from "@ui/PlayerSearch/PlayerSearchSelect";
 
-import styles from './ProfileCharts.module.scss'
+import type { PieChartProps } from "@common/types";
+import PieChart from "@components/ui/PieChart";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+
+import styles from "./ProfileCharts.module.scss";
 
 const PlayerComparison = ({ currentPlayerId }: { currentPlayerId: number }) => {
-  const [pieChartProps, setPieChartProps] = useState<PieChartProps | undefined>(undefined)
-  const [parent, _enableAnimations] = useAutoAnimate<HTMLDivElement>({ duration: 400 })
+  const [pieChartProps, setPieChartProps] = useState<PieChartProps | undefined>(
+    undefined,
+  );
+  const [parent, _enableAnimations] = useAutoAnimate<HTMLDivElement>({
+    duration: 400,
+  });
 
   return (
     <div ref={parent} className={styles.pieContainer}>
-      <PlayerSearchSelect currentPlayerId={currentPlayerId} setPieChartProps={setPieChartProps} />
+      <PlayerSearchSelect
+        currentPlayerId={currentPlayerId}
+        setPieChartProps={setPieChartProps}
+      />
       {pieChartProps && <PieChart {...pieChartProps} />}
     </div>
-  )
-}
+  );
+};
 
-export default PlayerComparison
+export default PlayerComparison;
