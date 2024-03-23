c34f8b7537185a147637dbe8c1164d3cd7086825
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-03-23 21:33:20 +0000

    Refactor season components

--- app/season/EditSeason.tsx
+++ app/season/EditSeason.tsx
@@ -1,32 +1,38 @@
-'use client'
+"use client";
 
-import { ButtonIcon } from 'app/components/ui/ButtonIcon'
-import React, { useState } from 'react'
-import { FiEdit2, FiX } from 'react-icons/fi'
+import { useState } from "react";
+import { FiEdit2, FiX } from "react-icons/fi";
 
-import styles from './Season.module.scss'
-import SeasonForm from './seasonForm'
+import { ButtonIcon } from "@ui/ButtonIcon";
+
+import { CreateSeasonForm } from "./CreateSeasonForm";
 
 type RenameSeasonProps = {
-  id: number
-}
+  id: number;
+};
 
 const EditSeason: React.FC<RenameSeasonProps> = ({ id }) => {
-  const [formOpen, setFormOpen] = useState(false)
+  const [formOpen, setFormOpen] = useState(false);
 
   if (!formOpen)
-    return <ButtonIcon Icon={FiEdit2} variation="destructive" onClick={() => setFormOpen(true)} />
+    return (
+      <ButtonIcon
+        Icon={FiEdit2}
+        variation="destructive"
+        onClick={() => setFormOpen(true)}
+      />
+    );
 
   return (
-    <div className={styles.editSeasonContainer}>
+    <div className="flex items-center gap-2">
       <ButtonIcon
         Icon={formOpen ? FiX : FiEdit2}
         variation="destructive"
-        onClick={() => setFormOpen(prev => !prev)}
+        onClick={() => setFormOpen((prev) => !prev)}
       />
-      {formOpen && <SeasonForm id={id} />}
+      {formOpen && <CreateSeasonForm id={id} />}
     </div>
-  )
-}
+  );
+};
 
-export default EditSeason
+export default EditSeason;
