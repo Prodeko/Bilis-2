15872134c57ba67a1d907e169159ed7fb88680af
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-04-01 19:49:06 +0000

    Refactor timeseries chart

--- app/components/ui/TimeSeriesChart/ToolTip.tsx
+++ app/components/ui/TimeSeriesChart/ToolTip.tsx
@@ -1,37 +1,48 @@
-import { TimeSeriesGame } from '@common/types'
+import { cva } from "class-variance-authority";
 
-import styles from './TimeSeriesChart.module.scss'
+import type { TimeSeriesGame } from "@common/types";
 
 interface Props {
-  gameData: TimeSeriesGame[]
-  dataPointIndex: number
+  gameData: TimeSeriesGame[];
+  dataPointIndex: number;
 }
 
+const colorStyles = cva("font-medium", {
+  variants: {
+    positive: {
+      true: "text-success-500",
+      false: "text-danger-500",
+    },
+  },
+});
+
 const Tooltip = ({ gameData, dataPointIndex }: Props) => {
-  const dataPoint = gameData[dataPointIndex]
-  const eloDiff = dataPoint.eloDiff
-  const opponentName = dataPoint?.opponent ? dataPoint.opponent : ''
+  const dataPoint = gameData[dataPointIndex];
+  const eloDiff = dataPoint.eloDiff;
+  const opponentName = dataPoint?.opponent ? dataPoint.opponent : "";
 
   return (
-    <div className={styles.arrow__box}>
-      <div className={styles.data__row}>
-        <span>Fargo: </span>
-        <span>{gameData[dataPointIndex].currentElo.toFixed(2)}</span>
+    <div className="relative flex flex-col gap-1 bg-neutral-800 p-2 text-lg">
+      <div className="flex justify-between gap-4">
+        <span className="font-bold">Fargo: </span>
+        <span className="font-medium">
+          {gameData[dataPointIndex].currentElo.toFixed(2)}
+        </span>
       </div>
-      <div className={styles.data__row}>
-        <span>Opponent: </span>
-        <span>{opponentName}</span>
+      <div className="flex justify-between gap-4">
+        <span className="font-bold">Opponent: </span>
+        <span className="font-medium">{opponentName}</span>
       </div>
-      <div className={styles.data__row}>
-        <span>Fargo Change: </span>
-        <span className={eloDiff >= 0 ? styles.positive : styles.negative}>
+      <div className="flex justify-between gap-4">
+        <span className="font-bold">Fargo Change: </span>
+        <span className={colorStyles({ positive: eloDiff >= 0 })}>
           {/* Add plus if positve */}
-          {eloDiff >= 0 && '+'}
+          {eloDiff >= 0 && "+"}
           {eloDiff.toFixed(2)}
         </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Tooltip
+export default Tooltip;
