14d61d7427b962cc81ed7d4929e4bf33b43bf765
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-04-02 13:59:58 +0000

    Refactor playerlayout

--- app/components/Player/PlayerLayout/Inner.tsx
+++ app/components/Player/PlayerLayout/Inner.tsx
@@ -1,11 +1,13 @@
-import { ReactNode } from 'react'
-
-import styles from './PlayerLayout.module.scss'
+import type { ReactNode } from "react";
 
 interface Props {
-  children: ReactNode
+  children: ReactNode;
 }
 
 export const PlayerLayoutInner = ({ children }: Props) => {
-  return <div className={styles.playerContainerInner}>{children}</div>
-}
+  return (
+    <div className="relative flex h-full items-center justify-center text-neutral-100">
+      {children}
+    </div>
+  );
+};
