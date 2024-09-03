86d12c87eff5a41bb66071a1e7fc943c51b72a87
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-09-03 16:22:30 +0000

    Add datatypes for table filters

--- src/app/components/ui/MultifunctionTable/Filter.tsx
+++ src/app/components/ui/MultifunctionTable/Filter.tsx
@@ -1,26 +1,24 @@
 import type { Dispatch, SetStateAction } from "react";
 
-import type { Column, Table as ReactTable } from "@tanstack/react-table";
+import type { Column } from "@tanstack/react-table";
 
 import { SelectFilter } from "./SelectFilter";
 
+type FilterDataType = "string" | "number" | "boolean";
+
 interface Props {
+  dataType: FilterDataType;
   column: Column<any, any>;
-  table: ReactTable<any>;
   setDisplayState: Dispatch<SetStateAction<number | string>>;
 }
 
-export const Filter = ({ column, table, setDisplayState }: Props) => {
-  const firstValue = table
-    .getPreFilteredRowModel()
-    .flatRows[0]?.getValue(column.id);
-
+export const Filter = ({ column, dataType, setDisplayState }: Props) => {
   const columnFilterValue = column.getFilterValue();
 
-  if (typeof firstValue === "string" && ["💩", " "].includes(firstValue)) {
+  if (dataType === "boolean") {
     return <SelectFilter column={column} setDisplayState={setDisplayState} />;
   }
-  if (typeof firstValue === "string") {
+  if (dataType === "string") {
     return (
       <input
         type="text"
@@ -34,37 +32,40 @@ export const Filter = ({ column, table, setDisplayState }: Props) => {
       />
     );
   }
+  if (dataType === "number") {
+    return (
+      <div className="flex w-full justify-between divide-[1px] divide-neutral-700 overflow-hidden">
+        <input
+          type="number"
+          inputMode="numeric"
+          value={(columnFilterValue as [number, number])?.[0] ?? ""}
+          onChange={(e) => {
+            column.setFilterValue((old: [number, number]) => [
+              e.target.value,
+              old?.[1],
+            ]);
+            setDisplayState(1);
+          }}
+          placeholder="Min"
+          className="w-full grow border border-neutral-700 bg-neutral-600 p-3 text-lg font-normal text-neutral-200 outline-none placeholder:text-neutral-800 "
+        />
+        <input
+          type="number"
+          inputMode="numeric"
+          value={(columnFilterValue as [number, number])?.[1] ?? ""}
+          onChange={(e) => {
+            column.setFilterValue((old: [number, number]) => [
+              old?.[0],
+              e.target.value,
+            ]);
+            setDisplayState(1);
+          }}
+          placeholder="Max"
+          className="w-full grow border border-neutral-700 bg-neutral-600 p-3 text-lg font-normal text-neutral-200 outline-none placeholder:text-neutral-800 "
+        />
+      </div>
+    );
+  }
 
-  return (
-    <div className="flex w-full justify-between divide-[1px] divide-neutral-700 overflow-hidden">
-      <input
-        type="number"
-        inputMode="numeric"
-        value={(columnFilterValue as [number, number])?.[0] ?? ""}
-        onChange={(e) => {
-          column.setFilterValue((old: [number, number]) => [
-            e.target.value,
-            old?.[1],
-          ]);
-          setDisplayState(1);
-        }}
-        placeholder="Min"
-        className="w-full grow border border-neutral-700 bg-neutral-600 p-3 text-lg font-normal text-neutral-200 outline-none placeholder:text-neutral-800 "
-      />
-      <input
-        type="number"
-        inputMode="numeric"
-        value={(columnFilterValue as [number, number])?.[1] ?? ""}
-        onChange={(e) => {
-          column.setFilterValue((old: [number, number]) => [
-            old?.[0],
-            e.target.value,
-          ]);
-          setDisplayState(1);
-        }}
-        placeholder="Max"
-        className="w-full grow border border-neutral-700 bg-neutral-600 p-3 text-lg font-normal text-neutral-200 outline-none placeholder:text-neutral-800 "
-      />
-    </div>
-  );
+  throw new Error("Invalid data type");
 };
--- src/app/components/ui/MultifunctionTable/schemas.ts
+++ src/app/components/ui/MultifunctionTable/schemas.ts
@@ -1,4 +1,4 @@
-import { ColumnDef, createColumnHelper } from "@tanstack/react-table";
+import { type ColumnDef, createColumnHelper } from "@tanstack/react-table";
 
 export interface PlayerTableSchema {
   position: number;
@@ -27,28 +27,46 @@ export const getGameColumnSchema = (): ColumnDef<GameTableSchema, any>[] => {
     columnHelper.accessor("time", {
       header: "time",
       cell: (info) => info.getValue(),
+      meta: {
+        dataType: "string",
+      },
     }),
     columnHelper.accessor("winner", {
       header: "winner",
       cell: (info) => info.getValue(),
+      meta: {
+        dataType: "string",
+      },
     }),
     columnHelper.accessor("winnerFargoNow", {
       id: "winnerEloAfter",
       header: "w.fargo",
       cell: (info) => info.getValue(),
+      meta: {
+        dataType: "number",
+      },
     }),
     columnHelper.accessor("loser", {
       header: "loser",
       cell: (info) => info.getValue(),
+      meta: {
+        dataType: "string",
+      },
     }),
     columnHelper.accessor("loserFargoNow", {
       id: "loserEloAfter",
       header: "l.fargo",
       cell: (info) => info.getValue(),
+      meta: {
+        dataType: "number",
+      },
     }),
     columnHelper.accessor("underTable", {
       header: "ut",
       cell: (info) => info.getValue(),
+      meta: {
+        dataType: "boolean",
+      },
     }),
   ];
 };
@@ -59,26 +77,44 @@ export const getPlayerColumnSchema = () => {
     columnHelper.accessor("position", {
       header: "pos",
       cell: (info) => info.getValue(),
+      meta: {
+        dataType: "number",
+      },
     }),
     columnHelper.accessor("fullName", {
       header: "name",
       cell: (info) => info.getValue(),
+      meta: {
+        dataType: "string",
+      },
     }),
     columnHelper.accessor("fargo", {
       header: "fargo",
       cell: (info) => info.getValue(),
+      meta: {
+        dataType: "number",
+      },
     }),
     columnHelper.accessor("gameCount", {
       header: "games",
       cell: (info) => info.getValue(),
+      meta: {
+        dataType: "number",
+      },
     }),
     columnHelper.accessor("winCount", {
       header: "wins",
       cell: (info) => info.getValue(),
+      meta: {
+        dataType: "number",
+      },
     }),
     columnHelper.accessor("winPercentage", {
       header: "win %",
       cell: (info) => info.getValue(),
+      meta: {
+        dataType: "number",
+      },
     }),
   ];
 };
